950362114955758b41eb73fb4b2b0f675b85ddeb
yogi <yogieabdulyusuf@gmail.com>
2018-07-14 13:33:43 +0700

    init

--- model/savings.py
+++ model/savings.py
@@ -5,3 +5,12 @@ class Savings(models.Model):
     _rec_name = 'name_savings_type'
 
     name_savings_type = fields.Char(string="Savings Type", required=True, )
+
+
+class SavingInterest(models.Model):
+    _name = 'savings.interest'
+    _rec_name = 'type_interest'
+    _description = 'Interest'
+
+    type_interest = fields.Char(string="Type of Interest", required=True, )
+    value_interest = fields.Float(string="Value of Interest",  required=True, )
