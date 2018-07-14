94409656b7c530f39ffe2b640eaeb30cd4485767
yogi <yogieabdulyusuf@gmail.com>
2018-07-14 16:12:53 +0700

    init

--- model/savings.py
+++ model/savings.py
@@ -14,3 +14,25 @@ class SavingInterest(models.Model):
 
     type_interest = fields.Char(string="Type of Interest", required=True, )
     value_interest = fields.Float(string="Value of Interest",  required=True, )
+
+
+class TransactionType(models.Model):
+    _name = 'transaction.type'
+    _rec_name = 'trans_type'
+    _description = 'Transaction Type'
+
+    trans_type = fields.Char(string="Transaction Type", required=True, )
+
+
+class SavingsTransaction(models.Model):
+    _name = 'savings.trans'
+    _rec_name = 'trans_number'
+    _description = 'Savings Transaction'
+
+    trans_number = fields.Integer(string="Transaction Number", required=True, )
+    date = fields.Date(string="Date", required=True, )
+    amount = fields.Float(string="Amount",  required=True, )
+    #state = fields.Selection([
+    #   ('open', 'Open'),
+    #   ('done', 'Done')
+    #], 'Status', default='draft' )
