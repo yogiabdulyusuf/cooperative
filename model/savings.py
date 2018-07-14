1c861874ef4244893a38174afde28fad16cfe7d6
yogiabdulyusuf <yogieabdulyusuf@gmail.com>
2018-07-14 21:27:09 +0700

    init

--- model/savings.py
+++ model/savings.py
@@ -1,12 +1,13 @@
 from odoo import api, fields, models
 
+# SAVINGS TYPE
 class Savings(models.Model):
     _name = 'savings.type'
     _rec_name = 'name_savings_type'
 
     name_savings_type = fields.Char(string="Savings Type", required=True, )
 
-
+# SAVINGS TYPE INTEREST
 class SavingInterest(models.Model):
     _name = 'savings.interest'
     _rec_name = 'type_interest'
@@ -15,7 +16,7 @@ class SavingInterest(models.Model):
     type_interest = fields.Char(string="Type of Interest", required=True, )
     value_interest = fields.Float(string="Value of Interest",  required=True, )
 
-
+# TRANSACTION TYPE
 class TransactionType(models.Model):
     _name = 'transaction.type'
     _rec_name = 'trans_type'
@@ -23,7 +24,7 @@ class TransactionType(models.Model):
 
     trans_type = fields.Char(string="Transaction Type", required=True, )
 
-
+# SAVINGS TRANSACTION
 class SavingsTransaction(models.Model):
     _name = 'savings.trans'
     _rec_name = 'trans_number'
@@ -36,3 +37,11 @@ class SavingsTransaction(models.Model):
     #   ('open', 'Open'),
     #   ('done', 'Done')
     #], 'Status', default='draft' )
+
+# SAVINGS ACCOUNT
+class SavingsAccount(models.Model):
+    _name = 'savings.account'
+    _rec_name = 'account_number'
+    _description = 'Savings Account'
+
+    account_number = fields.Char(string="Account Number", required=True, )
