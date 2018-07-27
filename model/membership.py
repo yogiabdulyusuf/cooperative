af0e06589a722e51f8c85d623bf1018e55a2d918
yogi <yogieabdulyusuf@gmail.com>
2018-07-27 17:17:14 +0700

    update hide field

--- model/membership.py
+++ model/membership.py
@@ -1,6 +1,11 @@
 from odoo import api, fields, models
 
+_logger = logging.getLogger(__name__)
+
+STATES = [('draft', 'Draft'), ('in_progress', 'In Progress'), ('active', 'Active'), ('done', 'Done')]
+
 class Membership(models.Model):
     _inherit = 'res.partner'
 
     active_member       = fields.Boolean(string="Active Member", )
+    state               = fields.Selection(string="", selection=STATES, required=True, default='draft')
