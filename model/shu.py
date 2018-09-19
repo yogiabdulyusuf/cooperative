b4ad16d61cd1c2e210f583c134cd6eb5d16e6069
yogi <yogieabdulyusuf@gmail.com>
2018-09-19 08:47:30 +0700

    Update SHU allocated

--- model/shu.py
+++ model/shu.py
@@ -16,14 +16,18 @@ class shuAllocated(models.Model):
 
 
     year_shu = fields.Integer(string="Year SHU", required=False, )
-    date = fields.Datetime(string="Date Today", required=False, )
+    date = fields.Datetime(string="Date today", required=False, )
     amount = fields.Float(string="Amount SHU",  required=False, )
     loan_services_percen = fields.Float(string="Loan Services %", required=False, )
     purchase_service_percen = fields.Float(string="Purchase Service %", required=False, )
-    deposit_services_percen = fields.Float(string="Deposit Services %", required=False, )
-    loan_services    = fields.Float(string=":", compute='calculate_loan_services', required=False, )
-    purchase_service = fields.Float(string=":", compute='calculate_purchase_service', required=False, )
-    deposit_services = fields.Float(string=":", compute='calculate_deposit_services', required=False, )
+    savings_services_percen = fields.Float(string="Savings Services %", required=False, )
+    loan_services    = fields.Float(string="Loan Services", compute='calculate_loan_services', required=False, )
+    purchase_service = fields.Float(string="Purchase Service", compute='calculate_purchase_service', required=False, )
+    savings_services = fields.Float(string="Savings Services", compute='calculate_deposit_services', required=False, )
+    debit = fields.Float(string="Calculate Debit 1 Year", default=0.0)
+    credit = fields.Float(string="Calculate Credit 1 Year", default=0.0)
+    debit_and_credit = fields.Float(string="Calculate Debit + Credit 1 Year", default=0.0)
+
 
 class journalSHU(models.Model):
     _name = 'journal.shu'
@@ -31,5 +35,7 @@ class journalSHU(models.Model):
 
     date = fields.Datetime(string="Date today", required=False, )
     shuallocated_id = fields.Many2one(comodel_name="shu.allocated", string="SHU", required=False, )
-
+    savings_list_id = fields.One2many(comodel_name="savings.trans", inverse_name="account_number_id",
+                                      string="Transactions")
+    state = fields.Selection(string="state", selection=[('open', 'Open'), ('done', 'Done'), ], required=False, default='open')
 
